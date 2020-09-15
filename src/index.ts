724231b6f53c1abafbb0e2a9abbc6d97b309bfdb
Matthew Miller <mmiller@mypassglobal.com>
2020-09-15 16:24:16 +0800

    simple rxjs snake game

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+import {fromEvent} from 'rxjs';
+import {gameOn} from './game'; // <- rxjs from NPM
+
+const svg: SVGSVGElement = document.getElementById('board') as unknown as SVGSVGElement;
+const startButton: HTMLButtonElement = document.getElementById('start') as HTMLButtonElement;
+
+fromEvent(startButton, 'click').subscribe(() => gameOn(svg));
